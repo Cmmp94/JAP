483130e0c02d4d2c20996c1d6d210a4f165aa496
Christian <christian.m.marsella@gmail.com>
2021-07-19 11:52:44 -0300

    Aca va todo lo de JaP

---
+++ Ejercicio 2/mi_script.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+function saludar(){
+	alert("Hola!, ¿cómo estás?");
+}
+
+function alegra(){
+	alert("Me alegra que hayas ingresado a mi consigna");
+}
+
+saludar();
+alegra();
---
+++ Trabajo entre pares 1/script.js
@@ -0,0 +1,5 @@
+function bienvenida(){
+	alert("Al darle aceptar, la página continuará cargando");
+}
+
+bienvenida();
